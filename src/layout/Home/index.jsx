cc048dfa809c90b918da3e761eb194c2df7e67b8
Stente3 <stentem0103@gmail.com>
2023-10-10 22:39:33 -0500

    Nuevo paquete fue agregado El paquete de 'material ui' fue agregado, además dentro del proyecto se añadio un pequeño ejemplo de uso

--- src/layout/Home/index.jsx
+++ src/layout/Home/index.jsx
@@ -1,7 +1,9 @@
+import Button from '@mui/material/Button'
+
 const Home = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <Button variant='contained'>Hello world</Button>
     </>
   )
 }
