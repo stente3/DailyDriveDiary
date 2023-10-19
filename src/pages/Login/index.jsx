30951fe0b75480d9454b8aa2496b90e3ecaa71c8
Stente3 <stentem0103@gmail.com>
2023-10-18 20:17:10 -0500

    Se agregó un archivo con "datos de usuarios" y se agregó una mini autentificación
    Se creó un archivo con extensión "json" en el cual se simula una base de datos de usuarios
    Se creó dentro de la página "login" una mini verificación de la existencia de los datos del usuario con respecto a la "base de datos ficticia"

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,50 @@
-const Login = () => {
-  return (
-    <>
-      <h1>Welcome to Login page</h1>
-    </>
-  )
-}
+import React, {useState} from "react";
 
-export { Login }
+const Login = () => {
+	const [userName, setUserName] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+
+		// Realiza la validación cargando el archivo JSON
+		try {
+			const response = await fetch("/data.json");
+			const data = await response.json();
+
+			const usuarioValido = data.find(
+				usuario =>
+					usuario.name === userName && usuario.password === password
+			);
+
+			if (usuarioValido) {
+				alert("Inicio de sesión exitoso");
+				// Realiza acciones adicionales, como redirigir al usuario.
+			} else {
+				alert("Credenciales incorrectas");
+			}
+		} catch (error) {
+			console.error("Error al cargar el archivo JSON:", error);
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='text'
+				placeholder='Nombre de usuario'
+				value={userName}
+				onChange={e => setUserName(e.target.value)}
+			/>
+			<input
+				type='password'
+				placeholder='Contraseña'
+				value={password}
+				onChange={e => setPassword(e.target.value)}
+			/>
+			<button type='submit'>Iniciar sesión</button>
+		</form>
+	);
+};
+
+export {Login};
