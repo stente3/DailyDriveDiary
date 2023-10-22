643044a8543b54467b0490c4912591386eb53951
softhamck <andreamunozcano5@gmail.com>
2023-10-22 16:57:35 -0500

    Por medio de React Router se agregó un Link que nos lleva hacia la página Advices

--- src/components/AdvicesButton/index.jsx
+++ src/components/AdvicesButton/index.jsx
@@ -1,8 +1,11 @@
-import styles from "./advicesButton.module.css"
+import { Link } from 'react-router-dom';
+import styles from './advicesButton.module.css';
 const AdvicesButton = () => (
-    <div>
-        <button className={styles.button}>Advices</button>
-    </div>
-  );
-  
-  export {AdvicesButton};
+	<div>
+		<button className={styles.button}>
+			<Link to='/advices'>Advices</Link>
+		</button>
+	</div>
+);
+
+export { AdvicesButton };
